d53f5af577d6a2486d3705f3b10d2bf2832a7ab2
Nathan-ma <than.matos@gmail.com>
2021-01-29 19:20:10 -0300

    v0.0.1

---
+++ Debug.h
@@ -0,0 +1,46 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <Arduino.h>
+#include "Macros.h"
+
+#ifndef PRINT_DEBUG
+#define PRINT_DEBUG true
+#endif
+
+#ifndef DEBUG_BUFFER_SIZE
+#define DEBUG_BUFFER_SIZE 1024 
+#endif
+
+class DebugClass {
+
+ private:
+  bool const debug = PRINT_DEBUG;
+  char const* TAG;
+
+ public:
+  /** @brief Class Constructor */
+  DebugClass(const char* ClassTag) : TAG(ClassTag){};
+
+  /** @brief Debug Print */
+  void print(const char* fmt, ...);
+  /** @brief Debug Override*/
+  void print(const __FlashStringHelper* fmt, ...);
+
+  /** @brief Error Print*/
+  void error(const char* fmt, ...);
+  /** @brief Error Override*/
+  void error(const __FlashStringHelper* fmt, ...);
+
+  /** @brief Warn Print*/
+  void warn(const char* fmt, ...);
+  /** @brief Warn Override*/
+  void warn(const __FlashStringHelper* fmt, ...);
+
+  /** @brief Warn Print*/
+  void detail(const char* fmt, ...);
+  /** @brief Warn Override*/
+  void detail(const __FlashStringHelper* fmt, ...);
+};
+
+#endif
